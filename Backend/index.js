2ee8e974770f240954817f99219c332047a2d5ea
shubham aswal <shubhamaswal7678@gmail.com>
2024-11-23 16:33:41 +0530

    added Frontend deployed URL

--- Backend/index.js
+++ Backend/index.js
@@ -29,11 +29,19 @@ dbConnect();
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(cors({
 
-    origin: "http://localhost:3000",
+    origin: "https://ede-techy.vercel.app",
     credentials: true,
 }));
+// app.use(cors({
+
+//     origin: "http://localhost:3000",
+//     credentials: true,
+// }));
+
+
 app.use(fileUpload({
     useTempFiles: true,
     tempFileDir: "/tmp",
